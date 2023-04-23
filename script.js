a5b4c8eb48d0d9fedb7bb71a748ac740231d9eb3
Manish <121147194+Manish68829@users.noreply.github.com>
2023-04-23 12:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+function updateUrl(){
+        var name=document.getElementById("name").value;
+        var year=document.getElementById("year").value;
+        var btn=document.getElementById("button");
+        var url=document.getElementById("url");
+        var nestedUrl="";
+        if(name&&year){
+            nestedUrl+=`?name=${name}&year=${year}`;}
+            else if(name){
+                nestedUrl+=`?name=${name}`;
+                
+            }
+            else{
+                nestedUrl+=`?year=${year}`;
+            }
+            url.textContent=url.textContent+nestedUrl;
+
+      }
